bae25099f1ea80d5013e80106eb4ab0585c3ad51
donvine mugendi <donvinemugendi@gmail.com>
2023-03-23 13:57:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+let cats=["Milo","Otis","Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function  destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(){
+    let copyOfCats=[...cats, 'Broom']
+    return copyOfCats;
+}
+function prependCat(){
+   var newCats=['Arnold', ...cats]
+    return newCats;
+}
+function removeLastCat(){
+    let copyOfNewCats=cats.slice(0,-1)
+    return copyOfNewCats;
+}
+function removeFirstCat(){
+    var newCatsString=cats.slice(1)
+    return newCatsString;
+}
+
+
+
+
+
